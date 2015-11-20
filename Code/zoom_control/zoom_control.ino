160709905b5bc1469d914a8936a779fbec9d87ca
Technicus <fgardner@madisoncollege.edu>
2015-11-20 13:04:07 -0600

    initial commit

---
+++ Code/zoom_control/zoom_control.ino
@@ -0,0 +1,64 @@
+
+#include <Servo.h> 
+ 
+Servo myservo;
+
+int pos = 90;
+float velocity = 1;
+
+#define out   9
+#define in  10
+
+#define outLimit 150
+#define inLimit  5
+
+
+void setup() 
+{ 
+  
+  Serial.begin(115200);
+  
+  myservo.attach(3);  // attaches the servo on pin 9 to the servo object 
+
+  pinMode(in, INPUT);
+  digitalWrite(in, 1);
+  
+  pinMode(out, INPUT);
+  digitalWrite(out, 1);
+
+} 
+ 
+void loop() 
+{ 
+                               
+  myservo.write(pos);              // tell servo to go to position in variable 'pos' 
+
+  if (digitalRead(in) == 0 and pos > inLimit) {
+    
+    pos -= velocity;
+    
+    velocity += .1;
+
+  }
+
+  if (digitalRead(out) == 0 and pos < outLimit) {
+    
+    pos += velocity;
+
+    velocity += .1;
+
+  }
+  
+  if (digitalRead(in) and digitalRead(out)) {
+  
+    velocity = 1;
+    
+  }
+  
+  delay(25);
+  
+  Serial.print(pos);
+  Serial.println(" ");
+
+} 
+
